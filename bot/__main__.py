d01a65e092b13b88e04f00882280f499cba7ab38
Root Gamer <96621049+Root-Gamer@users.noreply.github.com>
2022-01-13 19:10:48 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -43,20 +43,21 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n\n'\
-            f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
-            f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
-            f'<b>CPU:</b> {cpuUsage}%\n'\
-            f'<b>RAM:</b> {mem_p}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
-            f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
-            f'<b>Memory Total:</b> {mem_t}\n'\
-            f'<b>Memory Free:</b> {mem_a}\n'\
-            f'<b>Memory Used:</b> {mem_u}\n'
+    stats = f'╭───『🤖 𝑩𝒐𝒕 𝑺𝒕𝒂𝒕𝒊𝒄𝒔 🤖』\n│\n├─⏳<b>Bot Uptime:</b> {currentTime}\n│\n'\
+            f'├─💽<b>Storage:</b> {total}\n'\
+            f'├─💻<b>Used:</b> {used}\n├─💾<b>Free:</b> {free}\n'\
+            f'├─📤<b>Upload:</b> {sent}\n'\
+            f'├─📥<b>Download:</b> {recv}\n│\n'\
+            f'├─🖥️<b>CPU:</b> {cpuUsage}%\n'\
+            f'├─📏<b>RAM:</b> {mem_p}%\n'\
+            f'├─💿<b>DISK:</b> {disk}%\n│\n'\
+            f'├─🛰️<b>Physical Cores:</b> {p_core}\n'\
+            f'├─⚙️<b>Total Cores:</b> {t_core}\n'\
+            f'├─⚡<b>SWAP:</b> {swap_t}\n├─🔱<b>Used:</b> {swap_p}%\n│\n'\
+            f'├─💽<b>Memory Total:</b> {mem_t}\n'\
+            f'├─💾<b>Memory Free:</b> {mem_a}\n'\
+            f'├─💻<b>Memory Used:</b> {mem_u}\n│\n'\
+            f'╰───『💥 <a href="https://t.me/+SpnPh2Gc8kHwwAAF"><b>𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫</b></a> 💥』\n'
     sendMessage(stats, context.bot, update)
 
 
@@ -72,10 +73,10 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update, reply_markup)
+        sendMarkup('Supporters welcome😄\nHaters fuck off🤬🖕', context.bot, update, reply_markup)
 
 def restart(update, context):
-    restart_message = sendMessage("Restarting...", context.bot, update)
+    restart_message = sendMessage("Restarting  🔧", context.bot, update)
     if Interval:
         Interval[0].cancel()
     alive.kill()
@@ -250,11 +251,11 @@ def main():
     if ospath.isfile(".restartmsg"):
         with open(".restartmsg") as f:
             chat_id, msg_id = map(int, f)
-        bot.edit_message_text("Restarted successfully!", chat_id, msg_id)
+        bot.edit_message_text("╭──『♻️ 𝐁𝐎𝐓 𝐆𝐎𝐓 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ♻️』\n│\n├─🌀𝐑𝐞-𝐌𝐢𝐫𝐫𝐨𝐫 𝐘𝐨𝐮𝐫 𝐓𝐡𝐢𝐧𝐠\n│\n├─🖥️ 𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\n│\n╰──『💥 𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫 💥』", chat_id, msg_id)
         osremove(".restartmsg")
     elif OWNER_ID:
         try:
-            text = "<b>Bot Restarted!</b>"
+            text = "╭──『♻️ 𝐁𝐎𝐓 𝐆𝐎𝐓 𝐑𝐄𝐒𝐓𝐀𝐑𝐓𝐄𝐃 ♻️』\n│\n├─🌀𝐑𝐞-𝐌𝐢𝐫𝐫𝐨𝐫 𝐘𝐨𝐮𝐫 𝐓𝐡𝐢𝐧𝐠\n│\n├─🖥️ 𝐑𝐞𝐬𝐭𝐚𝐫𝐭𝐞𝐝\n│\n╰──『💥 𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫 💥』"
             bot.sendMessage(chat_id=OWNER_ID, text=text, parse_mode=ParseMode.HTML)
             if AUTHORIZED_CHATS:
                 for i in AUTHORIZED_CHATS:
