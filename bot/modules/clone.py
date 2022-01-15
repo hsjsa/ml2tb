c93dab4d6edfa3d0db0a6873204c52909a539be6
Root Gamer <96621049+Root-Gamer@users.noreply.github.com>
2022-01-15 11:27:34 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -83,7 +83,7 @@ def cloneNode(update, context):
                     update_all_messages()
             except IndexError:
                 pass
-        cc = f'\n│\n├─📬<b>cc: </b>{tag}\n│\n╰───『💥 <a href="https://t.me/+SpnPh2Gc8kHwwAAF"><b>𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫</b></a> 💥』'
+        cc = f'\n│\n├─📬<b>cc: </b>{tag}\n│\n╰───『💥 <a href="https://t.me/+FybEXzvlVJk0ZDE1"><b>𝐑𝐨𝐨𝐭 𝐆𝐚𝐦𝐞𝐫</b></a> 💥』'
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", context.bot, update)
         else:
