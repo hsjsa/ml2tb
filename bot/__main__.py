1d135ec55f3f0ab11007b3e0d12a9a1da060b4f9
hsjsa <101786584+hsjsa@users.noreply.github.com>
2022-03-28 20:37:07 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -20,7 +20,7 @@ from .helper.ext_utils.telegraph_helper import telegraph
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from .helper.telegram_helper.button_build import ButtonMaker
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, delete, speedtest, count, leech_settings, search, rss
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, count, leech_settings
 
 
 def stats(update, context):
